4be01f3164ded4981b661279001ac540d3b5b5f0
guaraqe <mail@guaraqe.gq>
2020-07-05 16:44:44 +0500

    Add SQL to global context

--- csdc-api/src/CSDC/SQL.hs
+++ csdc-api/src/CSDC/SQL.hs
@@ -1,6 +1,10 @@
 module CSDC.SQL
-  ( -- * Config
+  ( -- * Config and Secret
     Config (..)
+  , Secret (..)
+    -- * Context
+  , Context
+  , activate
     -- * Error
   , Error (..)
     -- * Action
@@ -9,9 +13,12 @@ module CSDC.SQL
   , query
   ) where
 
+import CSDC.Prelude
+
 import Control.Exception (Exception, finally, throwIO, try)
 import Control.Monad.IO.Class (MonadIO (..))
 import Control.Monad.Reader (ReaderT (..), MonadReader (..))
+import Data.Aeson (FromJSON (..), ToJSON (..))
 import Data.Text (Text)
 import Hasql.Connection (Connection, ConnectionError)
 import Hasql.Session (QueryError)
@@ -22,16 +29,37 @@ import qualified Hasql.Connection as Connection
 import qualified Hasql.Session as Session
 
 --------------------------------------------------------------------------------
--- Config
+-- Config and Secret
 
 -- | The configuration of the PostgreSQL server.
 data Config = Config
   { config_host :: Text
   , config_port :: Int
   , config_user :: Text
-  , config_password :: Text
   , config_database :: Text
-  } deriving (Show, Eq)
+  } deriving (Show, Eq, Generic)
+    deriving (FromJSON, ToJSON) via JSON Config
+
+-- | The secrets of the PostgreSQL server.
+data Secret = Secret
+  { secret_password :: Text
+  } deriving (Show, Eq, Generic)
+    deriving (FromJSON, ToJSON) via JSON Secret
+
+--------------------------------------------------------------------------------
+-- Context
+
+-- | The context of the PostgreSQL server.
+data Context = Context
+  { context_config :: Config
+  , context_secret :: Secret
+  } deriving (Show, Eq, Generic)
+    deriving (FromJSON, ToJSON) via JSON Context
+
+-- | Activate the configuration.
+-- TODO: Check the validity of the configuration.
+activate :: Config -> Secret -> IO Context
+activate config secret = pure $ Context config secret
 
 --------------------------------------------------------------------------------
 -- Error
@@ -52,14 +80,14 @@ newtype Action a = Action (ReaderT Connection IO a)
   deriving (Functor, Applicative, Monad)
 
 -- | Run a SQL action and return possible errors.
-run :: MonadIO m => Config -> Action a -> m (Either Error a)
-run config (Action m) = liftIO $ do
+run :: MonadIO m => Context -> Action a -> m (Either Error a)
+run (Context config secret) (Action m) = liftIO $ do
   let settings =
         Connection.settings
           (Text.Encoding.encodeUtf8 $ config_host config)
           (fromIntegral $ config_port config)
           (Text.Encoding.encodeUtf8 $ config_user config)
-          (Text.Encoding.encodeUtf8 $ config_password config)
+          (Text.Encoding.encodeUtf8 $ secret_password secret)
           (Text.Encoding.encodeUtf8 $ config_database config)
 
   Connection.acquire settings >>= \case
