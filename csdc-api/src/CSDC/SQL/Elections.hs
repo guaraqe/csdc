0295e0f1666a6c3c3dce359849b87dfba7c5a626
Klaranouba <armandguelina@gmail.com>
2023-04-05 17:57:34 +0200

    SQL implementation of routes

--- csdc-api/src/CSDC/SQL/Elections.hs
+++ csdc-api/src/CSDC/SQL/Elections.hs
@@ -24,48 +24,59 @@ insertElection :: Statement (Id Unit, NewElection) (Id Election)
 insertElection = Statement sql encoder decoder True
   where
     sql =
-      [sqlqq|
-      INSERT INTO election
-        ???????
-      VALUES
-        ???????
-      RETURNING
-        id
-      |]
+      ByteString.unlines
+        [ "INSERT INTO elections (unit, title, description, choices, election_type, visible_votes, ending_at, result, result_computed_at)",
+          "VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+          "RETURNING id"
+        ]
 
-    encoder = undefined
+    encoder = 
+      (contramap (.unit) Encoder.text)
+        <> (contramap (.title) Encoder.text)
+        <> (contramap (.description) Encoder.text)
+        <> (contramap (.choices) Encoder.text)
+        <> (contramap (.election_type) Encoder.text)
+        <> (contramap (.visible_votes) Encoder.text)
+        <> (contramap (.ending_at) Encoder.text)
+        <> (contramap (.result) Encoder.text)
+        <> (contramap (.result_computed_at) Encoder.text)
 
-    decoder = undefined
+    decoder = Decoder.singleRow Decoder.id
 
 selectElections :: Statement (Id Unit, Id Person) [ElectionInfo]
 selectElections = Statement sql encoder decoder True
   where
     sql =
-      [sqlqq|
-      SELECT
-        ???????
-      FROM
-        elections
-      WHERE
-        ???????
-      |]
+      ByteString.unlines
+        [ "SELECT unit, title, description, choices, election_type, visible_votes, ending_at, result, result_computed_at",
+          "FROM elections",
+          "WHERE id = $1"
+        ]
 
-    encoder =
-      contramap fst Encoder.id <>
-      contramap snd Encoder.id
+    encoder = Encoder.id
 
-    decoder = undefined
+    decoder = Decoder.rowMaybe $ do
+      unit <- Decoder.text
+      title <- Decoder.text
+      description <- Decoder.text
+      choices <- Decoder.text
+      election_type <- Decoder.text
+      visible_votes <- Decoder.text
+      ending_at <- Decoder.posixTime
+      result <- Decoder.text
+      result_computed_at <- Decoder.posixTime
+      pure Election {..}
 
 deleteElection :: Statement (Id Election) ()
 deleteElection = Statement sql encoder decoder True
   where
     sql =
-      [sqlqq|
-      DELETE FROM elections
-      WHERE ???????
-      |]
+      ByteString.unlines
+        [ "DELETE FROM elections",
+          "WHERE id = $1"
+        ]
 
-    encoder = undefined
+    encoder = Encoder.id
 
     decoder = Decoder.noResult
 
@@ -73,30 +84,32 @@ insertVoter :: Statement (Id Election, Id Person) ()
 insertVoter = Statement sql encoder decoder True
   where
     sql =
-      [sqlqq|
-      INSERT INTO voters
-        ???????
-      VALUES
-        ???????
-      |]
+      ByteString.unlines
+        [ "INSERT INTO voters (election, person, voted_at, vote)",
+          "VALUES ($1, $2, $3, $4)",
+          "RETURNING id"
+        ]
 
-    encoder = undefined
+    encoder =
+      (contramap (.electionId) Encoder.id)
+        <> (contramap (.personId) Encoder.id)
+        <> (contramap (.voted_at) Encoder.text)
+        <> (contramap (.voteId) Encoder.id)
 
-    decoder = Decoder.noResult
+    decoder = Decoder.singleRow Decoder.id
 
 insertVote :: Statement (Id Election, NewVote) (Id Vote)
 insertVote = Statement sql encoder decoder True
   where
     sql =
-      [sqlqq|
-      INSERT INTO voters
-        ???????
-      VALUES
-        ???????
-      RETURNING
-        id
-      |]
+      ByteString.unlines
+        [ "INSERT INTO votes (election, vote)",
+          "VALUES ($1, $2)",
+          "RETURNING id"
+        ]
 
-    encoder = undefined
+    encoder =
+      (contramap (.electionId) Encoder.id)
+        <> (contramap (.voted_at) Encoder.text)
 
-    decoder = undefined
+    decoder = Decoder.singleRow Decoder.id
