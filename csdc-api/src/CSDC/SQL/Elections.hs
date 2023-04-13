fdce0dd2a62ffcb740742a77342eb6a1b03fb889
Klaranouba <armandguelina@gmail.com>
2023-04-13 16:20:51 +0200

    dataBase merge

--- csdc-api/src/CSDC/SQL/Elections.hs
+++ csdc-api/src/CSDC/SQL/Elections.hs
@@ -17,6 +17,7 @@ import CSDC.Types.Election
 import CSDC.SQL.Decoder qualified as Decoder
 import CSDC.SQL.Encoder qualified as Encoder
 import CSDC.SQL.QQ
+import Data.ByteString.Char8 qualified as ByteString
 import Data.Functor.Contravariant (Contravariant (..))
 import Hasql.Statement (Statement (..))
 
@@ -24,63 +25,80 @@ insertElection :: Statement (Id Unit, NewElection) (Id Election)
 insertElection = Statement sql encoder decoder True
   where
     sql =
-      [sqlqq|
-      INSERT INTO election
-        ???????
-      VALUES
-        ???????
-      RETURNING
-        id
-      |]
+      ByteString.unlines
+        [ "INSERT INTO elections (unit, title, description, choices, election_type, visible_votes, ending_at, result, result_computed_at)",
+          "VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
+          "RETURNING id"
+        ]
 
-    encoder = undefined
+    -- XXX: fields have to be written like in the type definition, see
+    -- Types.Election
+    encoder =
+      contramap (.unitId) Encoder.id
+        <> contramap (.title) Encoder.text
+        <> contramap (.description) Encoder.text
+        <> contramap (.choices) Encoder.text
+        <> contramap (.election_type) Encoder.text
+        <> contramap (.visible_votes) Encoder.text
+        <> contramap (.ending_at) Encoder.text
+        <> contramap (.result) Encoder.text
+        <> contramap (.result_computed_at) Encoder.text
 
-    decoder = undefined
+    decoder = Decoder.singleRow Decoder.id 
 
 selectElections :: Statement (Id Unit, Id Person) [ElectionInfo]
 selectElections = Statement sql encoder decoder True
   where
     sql =
-      [sqlqq|
-      SELECT
-        ???????
-      FROM
-        elections
-      WHERE
-        ???????
-      |]
-
+     ByteString.unlines
+        [ "SELECT unit, title, description, choices, election_type, visible_votes, ending_at, result, result_computed_at",
+          "FROM elections",
+          "WHERE id = $1"
+        ]
+        
     encoder =
       contramap fst Encoder.id <>
       contramap snd Encoder.id
 
-    decoder = undefined
+    decoder = Decoder.rowList $ do
+      unit <- Decoder.text
+      title <- Decoder.text
+      description <- Decoder.text
+      choices <- Decoder.text
+      election_type <- Decoder.text
+      visible_votes <- Decoder.text
+      ending_at <- Decoder.posixTime
+      result <- Decoder.text
+      result_computed_at <- Decoder.posixTime
+      -- XXX: missing the info
+      pure ElectionInfo {..}
 
 deleteElection :: Statement (Id Election) ()
 deleteElection = Statement sql encoder decoder True
   where
     sql =
-      [sqlqq|
-      DELETE FROM elections
-      WHERE ???????
-      |]
+       ByteString.unlines
+        [ "DELETE FROM elections",
+          "WHERE id = $1"
+        ]
 
-    encoder = undefined
-
+    encoder = Encoder.id
     decoder = Decoder.noResult
 
 insertVoter :: Statement (Id Election, Id Person) ()
 insertVoter = Statement sql encoder decoder True
   where
     sql =
-      [sqlqq|
-      INSERT INTO voters
-        ???????
-      VALUES
-        ???????
-      |]
+      ByteString.unlines
+        [ "INSERT INTO voters (election, person, voted_at, vote)",
+          "VALUES ($1, $2, $3, $4)"
+        ]
 
-    encoder = undefined
+    encoder =
+      (contramap (.electionId) Encoder.id)
+        <> (contramap (.personId) Encoder.id)
+        <> (contramap (.voted_at) Encoder.text)
+        <> (contramap (.voteId) Encoder.id)
 
     decoder = Decoder.noResult
 
@@ -88,15 +106,14 @@ insertVote :: Statement (Id Election, NewVote) (Id Vote)
 insertVote = Statement sql encoder decoder True
   where
     sql =
-      [sqlqq|
-      INSERT INTO voters
-        ???????
-      VALUES
-        ???????
-      RETURNING
-        id
-      |]
+      ByteString.unlines
+        [ "INSERT INTO votes (election, vote)",
+          "VALUES ($1, $2)",
+          "RETURNING id"
+        ]
 
-    encoder = undefined
+    encoder =
+      (contramap (.electionId) Encoder.id)
+        <> (contramap (.voted_at) Encoder.text)
 
-    decoder = undefined
+    decoder = Decoder.singleRow Decoder.id
